a52109d41e09109aaca13df128162deda092d9e5
Amirhossein <4mirh03einhashemi@gmail.com>
2026-09-07 19:26:49 +0330

    refactor(user): surround query in executer

--- backend/src/modules/user/user.repository.ts
+++ backend/src/modules/user/user.repository.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "../../generated/prisma/internal/class.js";
+import { execute } from "../../utils/queryExecuter.js";
 
 export default class UserRepository {
   constructor(private readonly prisma: PrismaClient) {}
 
   async findByEmail(phone: string) {
-    return this.prisma.user.findUnique({ where: { phone } });
+    return execute(() => this.prisma.user.findUnique({ where: { phone } }));
   }
 }
