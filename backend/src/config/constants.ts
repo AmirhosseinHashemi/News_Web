3438786d4b854053f0cc754b4d62fa7a8ef25ecd
Amirhossein <4mirh03einhashemi@gmail.com>
2026-09-08 17:18:56 +0330

    fix(config): fix calculation refresh token expiration

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -3,4 +3,4 @@ import { env } from "./env.js";
 export const BCRYPT_SALT_ROUNDS = 12;
 
 export const REFRESH_TOKEN_EXPIRES_IN =
-  env.REFRESH_TOKEN_EXPIRES_IN_DAYS * 24 * 60 * 60 * 100;
+  env.REFRESH_TOKEN_EXPIRES_IN_DAYS * 24 * 60 * 60 * 1000;
