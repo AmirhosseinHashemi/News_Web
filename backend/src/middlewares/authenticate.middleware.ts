8d69902d6693b5008df43e27306ad8e83eb83d79
Amirhossein <4mirh03einhashemi@gmail.com>
2026-09-09 17:48:13 +0330

    feat(middlewares): create authenticate middleware

---
+++ backend/src/middlewares/authenticate.middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import UnauthorizedError from "../errors/UnauthorizedError.js";
+import { verifyAccessToken } from "../lib/jwt.js";
+
+export default function authenticateMiddleware(
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new UnauthorizedError();
+  }
+
+  const [type, token] = authHeader.split(" ");
+
+  if (type !== "Bearer" || !token) {
+    throw new UnauthorizedError();
+  }
+
+  const payload = verifyAccessToken(token);
+
+  req.user = {
+    id: payload.userId,
+  };
+
+  next();
+}
