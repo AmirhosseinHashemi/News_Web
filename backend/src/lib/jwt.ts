2a708721274c900d3284f86c71b60792d8273ff7
Amirhossein <4mirh03einhashemi@gmail.com>
2026-09-09 17:42:49 +0330

    feat(utils): create verify access token function

--- backend/src/lib/jwt.ts
+++ backend/src/lib/jwt.ts
@@ -1,12 +1,23 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { env } from "../config/env.js";
+import UnauthorizedError from "../errors/UnauthorizedError.js";
 
-type Payload = {
+type AccessTokenPayload = {
   userId: number;
 };
 
-export function generateAccessToken({ userId }: Payload) {
+export function generateAccessToken({ userId }: AccessTokenPayload) {
   return jwt.sign({ userId }, env.JWT_ACCESS_SECRET, {
     expiresIn: env.JWT_ACCESS_EXPIRES_IN,
   });
+}
+
+export function verifyAccessToken(token: string) {
+  try {
+    return jwt.verify(token, env.JWT_ACCESS_SECRET) as JwtPayload &
+      AccessTokenPayload;
+  } catch (error: unknown) {
+    console.log(error);
+    throw new UnauthorizedError();
+  }
 }
